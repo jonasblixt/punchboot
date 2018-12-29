d57f149a1e391aa83e5306acc3382460bad99e61
Jonas Persson <jonpe960@gmail.com>
2018-12-29 13:26:58 +0100

    Moved out boot code to separate module

--- src/main.c
+++ src/main.c
@@ -17,18 +17,14 @@
 #include <io.h>
 #include <image.h>
 #include <keys.h>
+#include <boot.h>
 
 void pb_main(void) 
 {
-    struct pb_pbi *pbi = NULL;
     uint32_t err = 0;
-    uint32_t boot_count = 0;
     uint32_t boot_part = 0;
-    uint32_t boot_lba_offset = 0;
-    uint32_t ts_init;
-    uint32_t ts0, ts1, ts11, ts2, ts3, ts4;
-
     bool flag_run_recovery = false;
+    struct pb_pbi *pbi = NULL;
 
     plat_wdog_init();
 
@@ -41,14 +37,11 @@ void pb_main(void)
         plat_reset();
     }
 
-    ts_init = plat_get_us_tick();
 
     LOG_INFO ("PB: " VERSION " starting...");
 
     if (gpt_init() != PB_OK)
         flag_run_recovery = true;
-    
-    ts0 = plat_get_us_tick();
 
     if (config_init() != PB_OK)
         flag_run_recovery = true;
@@ -73,64 +66,15 @@ void pb_main(void)
     if (flag_run_recovery)
         goto run_recovery;
 
-    ts1 = plat_get_us_tick();
 
-    switch (boot_part)
+    if (pb_boot_load_part((uint8_t) boot_part, &pbi) == PB_OK)
     {
-        case 0xAA:
-            LOG_INFO ("Loading from system A");
-            err = gpt_get_part_by_uuid(part_type_system_a, &boot_lba_offset);
-            if (err != PB_OK)
-                flag_run_recovery = true;
-        break;
-        case 0xBB:
-            LOG_INFO ("Loading from system B");
-            err = gpt_get_part_by_uuid(part_type_system_b, &boot_lba_offset);
-            if (err != PB_OK)
-                flag_run_recovery = true;
-        break;
-        default:
-            LOG_ERR("Invalid boot partition %lx", boot_part);
-            flag_run_recovery = true;
-    }
-    ts11 = plat_get_us_tick();
-
-    err = pb_image_load_from_fs(boot_lba_offset, &pbi);
-    
-    ts2 = plat_get_us_tick();
-
-    if (err != PB_OK)
-    {
-        LOG_ERR("Unable to load image, starting recovery");
+        LOG_INFO("Image on part %2.2X verified, booting...", (uint8_t) boot_part);
+        pb_boot_image(pbi);
+    } else {
+        LOG_ERR("Could not boot image on part %2.2X, entering recovery mode...",
+                        (uint8_t) boot_part);
         flag_run_recovery = true;
-    }
-
-    err = pb_image_verify(pbi, PB_KEY_DEV);
-    
-    ts3 = plat_get_us_tick();
-
-    if (err == PB_OK)
-    {
-        LOG_INFO("Image verified, booting...");
-
-        config_get_uint32_t(PB_CONFIG_BOOT_COUNT, &boot_count);
-        boot_count = boot_count + 1;
-        config_set_uint32_t(PB_CONFIG_BOOT_COUNT, boot_count);
-        config_commit();
-
-        ts4 = plat_get_us_tick();
-        asm("isb");
-        
-        tfp_printf ("Total: %lu us\n\r",ts4-ts_init);
-        tfp_printf ("gpt: %lu us\n\r", ts0-ts_init);
-        tfp_printf ("cfg: %lu us\n\r", ts1-ts0);
-        tfp_printf ("getpart: %lu us\n\r", ts11-ts1);
-        tfp_printf ("load %lu us\n\r", ts2-ts11);
-        tfp_printf ("vrfy: %lu us\n\r", ts3-ts2);
-        tfp_printf ("bc: %lu us\n\r", ts4-ts3);
-        tfp_printf ("ts_init: %lu us\n\r",ts_init);
-        
-        board_boot(pbi);
     }
 
 run_recovery:
