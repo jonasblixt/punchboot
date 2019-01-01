a7597e68842bfe397019dc60a52ac49264fd8ee1
Jonas Persson <jonpe960@gmail.com>
2019-01-01 17:13:33 +0100

    Generate signed binaries for imx6 plat

--- src/plat/imx6ul/makefile.mk
+++ src/plat/imx6ul/makefile.mk
@@ -10,6 +10,15 @@
 
 PB_ARCH_NAME = armv7a
 
+CST_TOOL ?= /work/cst_2.3.3
+
+SRK_TBL  ?= $(shell realpath ../pki/imx6ul_hab_testkeys/SRK_1_2_3_4_table.bin)
+CSFK_PEM ?= $(shell realpath ../pki/imx6ul_hab_testkeys/CSF1_1_sha256_4096_65537_v3_usr_crt.pem)
+IMG_PEM  ?= $(shell realpath ../pki/imx6ul_hab_testkeys/IMG1_1_sha256_4096_65537_v3_usr_crt.pem)
+
+PB_CSF_TEMPLATE = plat/imx6ul/pb.csf.template
+SED = $(shell which sed)
+
 PLAT_C_SRCS  += plat/imx6ul/plat.c
 PLAT_C_SRCS  += plat/imx6ul/imx_uart.c
 PLAT_C_SRCS  += plat/imx6ul/ehci.c
@@ -19,6 +28,20 @@ PLAT_C_SRCS  += plat/imx6ul/gpt.c
 PLAT_C_SRCS  += plat/imx6ul/caam.c
 PLAT_C_SRCS  += plat/imx6ul/ocotp.c
 PLAT_C_SRCS	 += plat/imx6ul/wdog.c
+PLAT_C_SRCS  += plat/imx6ul/hab.c
 
 plat_clean:
 	@-rm -rf plat/imx6ul/*.o
+
+
+plat_final:
+	$(eval PB_FILESIZE=$(shell stat -c%s "pb.imx"))
+	$(eval PB_FILESIZE_HEX=0x$(shell echo "obase=16; $(PB_FILESIZE)" | bc	))
+	$(eval PB_CST_ADDR=0x$(shell echo "obase=16; $$(( $(PB_ENTRY) - 0xC00 ))" | bc	))
+	@echo "PB imx image size: $(PB_FILESIZE) bytes ($(PB_FILESIZE_HEX)), cst addr $(PB_CST_ADDR)"
+	@$(SED) -e 's/__BLOCKS__/Blocks = $(PB_CST_ADDR) 0x000 $(PB_FILESIZE_HEX) "pb.imx"/g' < $(PB_CSF_TEMPLATE) > pb.csf
+	@$(SED) -i -e 's#__SRK_TBL__#$(SRK_TBL)#g' pb.csf
+	@$(SED) -i -e 's#__CSFK_PEM__#$(CSFK_PEM)#g' pb.csf
+	@$(SED) -i -e 's#__IMG_PEM__#$(IMG_PEM)#g'  pb.csf
+	@$(CST_TOOL) --o pb_csf.bin --i pb.csf
+	@cat pb.imx pb_csf.bin > pb_signed.imx
