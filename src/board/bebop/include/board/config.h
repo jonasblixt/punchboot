6a2d37f3a1e210a56d2b0efdd82e2ba948242a00
Jonas Persson <jonpe960@gmail.com>
2019-01-23 15:37:43 +0100

    misc

---
+++ src/board/bebop/include/board/config.h
@@ -0,0 +1,22 @@
+/**
+ * Punch BOOT
+ *
+ * Copyright (C) 2018 Jonas Persson <jonpe960@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+
+
+#ifndef __BOARD_CONFIG__
+#define __BOARD_CONFIG__
+
+
+
+#define EHCI_PHY_BASE 0x02184000
+
+#define PB_MAX_FAIL_BOOT_COUNT 16
+
+
+#endif
