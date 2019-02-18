3d5e0db89f5e1ab501a211ee40b24af990877ca1
Jonas Persson <jonpe960@gmail.com>
2019-02-18 14:45:17 +0100

    Added more tests that corrupts GPT tables in various ways

---
+++ src/tests/test_corrupt_gpt4.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+source tests/common.sh
+wait_for_qemu_start
+
+# After a reboot, bootloader should have recovered from backup GPT tables
+sync
+sgdisk -v /tmp/disk | grep "No problems found"
+result_code=$?
+
+if [ $result_code -ne 0 ];
+then
+    test_end_error
+fi
+
+# Trash GPT partition header
+
+dd if=/dev/urandom of=/tmp/disk bs=512 skip=2 conv=notrunc count=3
+
+# Reset
+$PB boot -r
+result_code=$?
+
+if [ $result_code -ne 0 ];
+then
+    test_end_error
+fi
+
+test_end_ok
---
+++ src/tests/test_corrupt_gpt5.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+source tests/common.sh
+wait_for_qemu_start
+
+# After a reboot, bootloader should have recovered from backup GPT tables
+sync
+sgdisk -v /tmp/disk | grep "No problems found"
+result_code=$?
+
+if [ $result_code -ne 0 ];
+then
+    test_end_error
+fi
+
+# Reset
+$PB boot -r
+result_code=$?
+
+if [ $result_code -ne 0 ];
+then
+    test_end_error
+fi
+
+test_end_ok
