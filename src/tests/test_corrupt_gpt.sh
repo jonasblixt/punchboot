211c8bef57b3a075ee7d4f7630f6110130e04bf7
Jonas Persson <jonpe960@gmail.com>
2019-04-13 17:20:19 +0200

    ...

--- src/tests/test_corrupt_gpt.sh
+++ src/tests/test_corrupt_gpt.sh
@@ -3,10 +3,18 @@ sync
 source tests/common.sh
 wait_for_qemu_start
 
+# Reset
+$PB boot -r
+result_code=$?
+
+if [ $result_code -ne 0 ];
+then
+    test_end_error
+fi
 
 # First make sure that the current image is OK
 sync
-sgdisk -v /tmp/disk | grep "No problems found"
+sgdisk -v /tmp/disk
 result_code=$?
 
 if [ $result_code -ne 0 ];
@@ -18,8 +26,7 @@ fi
 
 dd if=/dev/urandom of=/tmp/disk conv=notrunc bs=512 count=64
 sync
-sleep 0.5
-sync
+
 sgdisk -v /tmp/disk
 result_code=$?
 
@@ -28,14 +35,6 @@ then
     test_end_error
 fi
 
-# Reset
-$PB boot -r
-result_code=$?
-
-if [ $result_code -ne 0 ];
-then
-    test_end_error
-fi
 
 test_end_ok
 sync
--- src/tests/test_corrupt_gpt2.sh
+++ src/tests/test_corrupt_gpt2.sh
@@ -2,6 +2,16 @@
 source tests/common.sh
 wait_for_qemu_start
 
+
+# Reset
+$PB boot -r
+result_code=$?
+
+if [ $result_code -ne 0 ];
+then
+    test_end_error
+fi
+
 # After a reboot, bootloader should have recovered from backup GPT tables
 sync
 sgdisk -v /tmp/disk | grep "No problems found"
@@ -12,13 +22,5 @@ then
     test_end_error
 fi
 
-# Reset
-$PB boot -r
-result_code=$?
-
-if [ $result_code -ne 0 ];
-then
-    test_end_error
-fi
 
 test_end_ok
