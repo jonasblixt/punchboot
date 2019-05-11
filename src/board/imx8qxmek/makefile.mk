2496f24478f91ab4c95f360aafee03e1f55a8a82
Jonas Persson <jonpe960@gmail.com>
2019-05-11 10:16:57 +0200

    Add: Add all public EC test keys as default

--- src/board/imx8qxmek/makefile.mk
+++ src/board/imx8qxmek/makefile.mk
@@ -10,9 +10,9 @@ BOARD_C_SRCS += boot/atf_linux.c
 MKIMAGE         = mkimage_imx8
 FINAL_IMAGE     = $(TARGET).imx
 
-#KEYS = ../pki/secp384r1-pub-key.der
-KEYS  = ../pki/secp384r1-pub-key.der
-#KEYS = ../pki/dev_rsa_public.der
+KEYS  = ../pki/secp256r1-pub-key.der
+KEYS += ../pki/secp384r1-pub-key.der
+KEYS += ../pki/secp521r1-pub-key.der
 #KEYS += ../pki/field1_rsa_public.der
 #KEYS += ../pki/field2_rsa_public.der
 
