69c82dd2315a635cd54ad0d5a4909724917a8fa1
Jonas Persson <jonpe960@gmail.com>
2019-05-11 15:18:19 +0200

    Bugfix: test suite is not stopped if module tests fail
    Descriptor updated with current VID/PID

--- src/tests/makefile.mk
+++ src/tests/makefile.mk
@@ -86,7 +86,7 @@ test: $(ARCH_OBJS) $(TEST_OBJS)
 			 $(LIBS) $(TEST).o -o $(TEST) && \
 		echo "TEST $(TEST)"  >> test_log.txt && \
 		sleep 0.1 && \
-		$(QEMU) $(QEMU_FLAGS) -kernel $(TEST) >> test_log.txt 2>&1; )
+		$(QEMU) $(QEMU_FLAGS) -kernel $(TEST) >> test_log.txt 2>&1 || exit; )
 
 	@echo
 	@echo
