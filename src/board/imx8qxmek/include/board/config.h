c950f357da70b64b68152b4de26e28e2ad911526
Jonas Persson <jonpe960@gmail.com>
2019-10-15 14:22:39 +0200

    imx8x: remove earlyprintk

--- src/board/imx8qxmek/include/board/config.h
+++ src/board/imx8qxmek/include/board/config.h
@@ -1,30 +1,11 @@
 #ifndef __BOARD_CONFIG_H__
 #define __BOARD_CONFIG_H__
 
-
-            /*"earlycon=lpuart32,0x5a060000,115200 earlyprintk " \*/
-/*"fec.macaddr=0xe2,0xf4,0x91,0x3a,0x82,0x93 " \*/
-
-/*
-#define BOARD_BOOT_ARGS "console=ttyLP0,115200  " \
-                        "quiet " \
-                        "fec.macaddr=0xe2,0xf4,0x91,0x3a,0x82,0x93 " \
-                        "root=PARTUUID=%s " \
-                        "ro rootfstype=squashfs gpt rootwait"
-
-#define BOARD_BOOT_ARGS_VERBOSE "console=ttyLP0,115200  " \
-                        "earlycon=lpuart32,0x5a060000,115200 earlyprintk " \
-                        "fec.macaddr=0xe2,0xf4,0x91,0x3a,0x82,0x93 " \
-                        "root=PARTUUID=%s " \
-                        "ro rootfstype=squashfs gpt rootwait"
-*/
-
-
 #define BOARD_BOOT_ARGS "console=ttyLP0,115200  " \
                         "quiet " \
                         "fec.macaddr=0xe2,0xf4,0x91,0x3a,0x82,0x93 "
 
 #define BOARD_BOOT_ARGS_VERBOSE "console=ttyLP0,115200  " \
                         "earlycon=adma_lpuart32,0x5a060000,115200 earlyprintk " \
-                        "fec.macaddr=0xe2,0xf4,0x91,0x3a,0x82,0x93 " 
+                        "fec.macaddr=0xe2,0xf4,0x91,0x3a,0x82,0x93 "
 #endif
