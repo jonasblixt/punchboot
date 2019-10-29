f2bf9fabd9e03f2b4179067a5e8b08cdbea89e85
Jonas Persson <jonpe960@gmail.com>
2019-10-29 13:26:12 +0100

    Update license headers

--- src/include/pb/timing_report.h
+++ src/include/pb/timing_report.h
@@ -2,7 +2,7 @@
 /**
  * Punch BOOT
  *
- * Copyright (C) 2018 Jonas Persson <jonpe960@gmail.com>
+ * Copyright (C) 2018 Jonas Blixt <jonpe960@gmail.com>
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *
--- src/plat/test/makefile.mk
+++ src/plat/test/makefile.mk
@@ -1,7 +1,7 @@
 #
 # Punch BOOT
 #
-# Copyright (C) 2018 Jonas Persson <jonpe960@gmail.com>
+# Copyright (C) 2018 Jonas Blixt <jonpe960@gmail.com>
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
