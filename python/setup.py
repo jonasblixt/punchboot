7f5257158cefb76788b457aea421c6c9e786f66f
Jonas Blixt <jonpe960@gmail.com>
2020-05-11 16:26:15 +0200

    Separate python build for now

--- python/setup.py
+++ python/setup.py
@@ -6,27 +6,12 @@ import os
 import setuptools
 import platform
 from setuptools import find_packages
-from setuptools.command.egg_info import egg_info
-
-SRC_PATH = os.path.dirname(__file__)
-
-class EggInfoCommand(egg_info):
-    def run(self):
-        if "build" in self.distribution.command_obj:
-            build_command = self.distribution.command_obj["build"]
-
-            self.egg_base = build_command.build_base
-
-            self.egg_info = os.path.join(self.egg_base, os.path.basename(self.egg_info))
-
-        egg_info.run(self)
 
 def setup(ext_modules):
     setuptools.setup(
         name='punchboot',
         version="0.1.0",
         description=('Python wrapper for the punchboot library'),
-        package_dir={"": SRC_PATH,},
         long_description="blerp", #open('../README.rst', 'r').read(),
         author='Jonas Blixt',
         author_email='jonpe960@gmail.com',
@@ -38,6 +23,21 @@ def setup(ext_modules):
         url='https://github.com/jonasblixt/punchboot-tools',
         ext_modules=ext_modules,
         test_suite="tests",
-        cmdclass={"egg_info": EggInfoCommand,})
+        )
 
-setup(None)
+setup([setuptools.Extension(name='punchboot.punchboot',
+                            sources=[
+                                'pb_wrapper.c',
+                            ],
+                            include_dirs=[
+                                '../include',
+                            ],
+                            libraries=[
+                                'punchboot',
+                                'usb-1.0',
+                                'bpak',
+                            ],
+                            library_dirs=[
+                                'lib/.libs'
+                            ],
+                            )])
