26f461316beab845b37b79b8dcbf4703bfbf1658
Jonas Blixt <jonpe960@gmail.com>
2020-09-02 21:10:49 +0200

    pbstate: Dont use ioctl to get size
    the ioctl call does not work on normal files and breaks the punchboot
    test suite.
    
    Do not rely on the GTP block offset pointer for the actual GPT table, in
    reality it always follows the header and is always 128 entries.

--- tools/pbstate/src/gpt.c
+++ tools/pbstate/src/gpt.c
@@ -123,7 +123,11 @@ int gpt_part_by_uuid(struct gpt_table *gpt, const char *uuid_str,
         i += 2;
     }
 
-    printf("%s -> %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\n", uuid_str, uuid[0], uuid[2], uuid[2], uuid[3], uuid[4], uuid[5], uuid[6], uuid[7], uuid[8], uuid[9], uuid[10], uuid[11], uuid[12], uuid[13], uuid[14], uuid[15]);
+    //printf("%s -> %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\n",
+    //              uuid_str, uuid[0], uuid[2], uuid[2], uuid[3], uuid[4],
+    //                        uuid[5], uuid[6], uuid[7], uuid[8], uuid[9],
+    //                        uuid[10], uuid[11], uuid[12], uuid[13], uuid[14],
+    //                        uuid[15]);
 
     uuid_to_guid((uint8_t *) uuid, guid);
 
@@ -148,7 +152,6 @@ int gpt_init(const char *device, struct gpt_table **gpt_)
 {
     struct gpt_table *gpt = malloc(sizeof(struct gpt_table));
     int rc = GPT_OK;
-    uint64_t no_of_blocks;
     off_t offset;
     size_t sz;
     int fd;
@@ -168,12 +171,6 @@ int gpt_init(const char *device, struct gpt_table **gpt_)
         goto err_free_out;
     }
 
-    if (ioctl(fd, BLKGETSIZE, &no_of_blocks) != 0)
-    {
-        rc = -GPT_ERROR;
-        goto err_close_fd_out;
-    }
-
     /* Read primary and backup GPT headers and parition tables */
 
     if (lseek(fd, 512, SEEK_SET) != 512)
@@ -185,13 +182,6 @@ int gpt_init(const char *device, struct gpt_table **gpt_)
     sz = read(fd, &gpt->hdr, sizeof(gpt->hdr));
 
     if (sz != sizeof(gpt->hdr))
-    {
-        rc = -GPT_ERROR;
-        goto err_close_fd_out;
-    }
-
-    if ((unsigned int)lseek(fd, gpt->hdr.entries_start_lba*512, SEEK_SET) != \
-                  gpt->hdr.entries_start_lba*512)
     {
         rc = -GPT_ERROR;
         goto err_close_fd_out;
@@ -211,18 +201,15 @@ int gpt_init(const char *device, struct gpt_table **gpt_)
         goto ok_close_fd_out;
     }
 
+primary_failed:
     /* Try backup table */
 
-    offset = no_of_blocks*512 - (sizeof(struct gpt_header)/512);
-
-    if (lseek(fd, offset, SEEK_SET) != offset)
+    if (lseek(fd, -(sizeof(struct gpt_header) + 128*sizeof(struct gpt_part_hdr)),
+                 SEEK_END) == (off_t) -1)
     {
         rc = -GPT_ERROR;
         goto err_close_fd_out;
     }
-
-    if ((unsigned int)lseek(fd, gpt->hdr.entries_start_lba*512, SEEK_SET) != \
-                  gpt->hdr.entries_start_lba*512)
 
     sz = read(fd, gpt->part, sizeof(struct gpt_part_hdr)*128);
 
@@ -247,7 +234,6 @@ int gpt_init(const char *device, struct gpt_table **gpt_)
     }
 
     rc = GPT_OK;
-
 ok_close_fd_out:
     close(fd);
     return rc;
