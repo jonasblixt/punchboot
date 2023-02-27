dea4dc53c9174771396f52562ab6ec67f717e601
Jonas Blixt <jonpe960@gmail.com>
2023-02-27 10:36:48 +0100

    pb-tools: Version v0.3.2

--- include/pb-tools/pb-tools.h
+++ include/pb-tools/pb-tools.h
@@ -3,8 +3,8 @@
 
 #define PB_TOOLS_VERSION_MAJOR 0
 #define PB_TOOLS_VERSION_MINOR 3
-#define PB_TOOLS_VERSION_PATCH 1
-#define PB_TOOLS_VERSION_STRING "0.3.1"
+#define PB_TOOLS_VERSION_PATCH 2
+#define PB_TOOLS_VERSION_STRING "0.3.2"
 
 #define PB_EXPORT __attribute__((visibility("default")))
 
