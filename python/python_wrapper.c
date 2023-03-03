9d700ff313551bcb49b56c590bfaf4b524c95ece
Jonas Blixt <jonpe960@gmail.com>
2023-03-03 15:54:26 +0100

    build: Use more aggressive compiler settings

--- python/python_wrapper.c
+++ python/python_wrapper.c
@@ -486,7 +486,7 @@ static PyObject* part_list_partitions(PyObject* self, PyObject* Py_UNUSED(args))
         uuid_unparse(tbl[i].uuid, uuid_str);
 
         if (!use_description) {
-            snprintf(entryname, sizeof(entryname), "Part%d", i);
+            snprintf(entryname, sizeof(entryname), "Part%d", i );
         }
 
         ret = PyDict_SetItemString(part_dict,
