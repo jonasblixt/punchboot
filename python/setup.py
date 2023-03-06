8117b739d74417d64f44e50056c80b44fe58f62f
Jonas Blixt <jonpe960@gmail.com>
2023-03-06 15:53:09 +0100

    python: Use setuptools for python installation
    CMakes python/find_package does not work well in all environments.
    Using python's setuptools seems to be the only way forward.
    
    This commit removes the 'PB_TOOLS_BUILD_PYTHON_WRAPPER' option but keeps
    the CMakefile under python/ for testing. This way the extension is built
    by cmake when running tests and we can link against the locally built
    library.

---
+++ python/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+from setuptools import Extension
+import re
+
+def read_version():
+    with open("../include/pb-tools/pb-tools.h", "r") as f:
+        return re.search(r"PB_TOOLS_VERSION_STRING \"(.*)\"$",
+                         f.read(),
+                         re.MULTILINE).group(1)
+
+setup(name='punchboot',
+      version=read_version(),
+      description="Punchboot tools python wrapper",
+      author="Jonas Blixt",
+      author_email="jonpe960@gmail.com",
+      license="BSD",
+      url="https://github.com/jonasblixt/punchboot-tools",
+      ext_modules=[
+          Extension(name="punchboot",
+                        sources=[
+                            "python_wrapper.c",
+                            "../src/sha256.c",
+                            "../src/crc.c",
+                            "../src/uuid/parse.c",
+                            "../src/uuid/unparse.c",
+                            "../src/uuid/compare.c",
+                            "../src/uuid/pack.c",
+                            "../src/uuid/unpack.c"
+                        ],
+                    libraries=["punchboot"],
+                    include_dirs=["../src"],
+                    )
+      ],
+)
