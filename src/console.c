7146fa911bdc157fb55aa28d5107979800389390
Jonas Blixt <jonpe960@gmail.com>
2023-03-19 21:32:07 +0100

    drivers/console: Add simple console driver
    Instead of having plat_ -functions in each platform.

---
+++ src/console.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <console.h>
+#include <config.h>
+#include <pb/plat.h>
+
+static const struct console_ops *ops;
+static uintptr_t base;
+
+void console_init(uintptr_t base_, const struct console_ops *ops_)
+{
+    ops = ops_;
+    base = base_;
+
+#ifdef CONFIG_PRINT_BOOT_BANNER
+    printf("\n\rPB " PB_VERSION ", %s (%i)\n\r", plat_boot_reason_str(),
+                                                 plat_boot_reason());
+#endif
+}
+
+void console_putc(char c)
+{
+    if (ops->putc) {
+        ops->putc(base, c);
+    }
+}
