11ffe8b6935d0516989ab07357cb7eb9071558ec
Jonas Blixt <jonpe960@gmail.com>
2023-03-19 22:11:13 +0100

    libc: add strncmp

--- src/lib/libc/strcmp.c
+++ src/lib/libc/strcmp.c
@@ -50,3 +50,19 @@ strcmp(const char *s1, const char *s2)
             return (0);
     return (*(const unsigned char *)s1 - *(const unsigned char *)(s2 - 1));
 }
+
+int
+strncmp(const char *s1, const char *s2, size_t n)
+{
+
+	if (n == 0)
+		return (0);
+	do {
+		if (*s1 != *s2++)
+			return (*(const unsigned char *)s1 -
+				*(const unsigned char *)(s2 - 1));
+		if (*s1++ == '\0')
+			break;
+	} while (--n != 0);
+	return (0);
+}
