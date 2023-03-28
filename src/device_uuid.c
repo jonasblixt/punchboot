cea00c22c0c6b5a70da6e8f161e3914f7792bf8d
Jonas Blixt <jonpe960@gmail.com>
2023-03-28 08:34:53 +0200

    device_uuid: New API
    Separate the device UUID from the platfrom API and make it optional.

---
+++ src/device_uuid.c
@@ -0,0 +1,31 @@
+#include <pb/errors.h>
+#include <pb/plat.h>
+#include <device_uuid.h>
+#include <uuid.h>
+#include <platform_defs.h>
+
+static uuid_t device_uu;
+
+int device_uuid(uuid_t uu)
+{
+    int rc;
+    uint8_t plat_unique[16];
+    size_t plat_unique_len = sizeof(plat_unique);
+
+    if (uuid_is_null(device_uu)) {
+        rc = plat_get_unique_id(plat_unique, &plat_unique_len);
+
+        if (rc != PB_OK)
+            return rc;
+
+        rc = uuid_gen_uuid3(PLATFORM_NS_UUID,
+                            (const char *) plat_unique, plat_unique_len,
+                            device_uu);
+
+        if (rc != 0)
+            return rc;
+    }
+
+    uuid_copy(uu, device_uu);
+    return PB_OK;
+}
