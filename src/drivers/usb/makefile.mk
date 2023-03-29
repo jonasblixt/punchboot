26dcbdfea22d9e0311f90a82b905284eb20e53af
Jonas Blixt <jonpe960@gmail.com>
2023-03-29 13:22:28 +0200

    drivers/usb: Add imx usb2 phy driver

--- src/drivers/usb/makefile.mk
+++ src/drivers/usb/makefile.mk
@@ -1,2 +1,3 @@
 src-$(CONFIG_DRIVER_USB_DEVICE) += src/drivers/usb/usb_core.c
 src-$(CONFIG_DRIVER_IMX_EHCI) += src/drivers/usb/imx_ehci.c
+src-$(CONFIG_DRIVER_IMX_USB2_PHY) += src/drivers/usb/imx_usb2_phy.c
