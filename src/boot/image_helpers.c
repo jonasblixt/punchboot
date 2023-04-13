1885b9b46c25979ec62836f1fa5cd250db9eb6f5
Jonas Blixt <jonpe960@gmail.com>
2023-04-13 12:36:36 +0200

    boot/image_helper: Put back the overlap checker
    This checks if an images tries to write to writable pb memory areas.

--- src/boot/image_helpers.c
+++ src/boot/image_helpers.c
@@ -14,7 +14,21 @@
 #include <pb/plat.h>
 #include <pb/plat.h>
 #include <bpak/id.h>
+#include <bpak/keystore.h>
 #include <boot/image_helpers.h>
+
+IMPORT_SYM(uintptr_t, _code_start, code_start);
+IMPORT_SYM(uintptr_t, _code_end, code_end);
+IMPORT_SYM(uintptr_t, _data_region_start, data_start);
+IMPORT_SYM(uintptr_t, _data_region_end, data_end);
+IMPORT_SYM(uintptr_t, _ro_data_region_start, ro_data_start);
+IMPORT_SYM(uintptr_t, _ro_data_region_end, ro_data_end);
+IMPORT_SYM(uintptr_t, _stack_start, stack_start);
+IMPORT_SYM(uintptr_t, _stack_end, stack_end);
+IMPORT_SYM(uintptr_t, _zero_region_start, bss_start);
+IMPORT_SYM(uintptr_t, _zero_region_end, bss_end);
+IMPORT_SYM(uintptr_t, _no_init_start, no_init_start);
+IMPORT_SYM(uintptr_t, _no_init_end, no_init_end);
 
 extern struct bpak_keystore keystore_pb;
 static uint8_t signature[512];
@@ -29,6 +43,7 @@ int boot_image_auth_header(struct bpak_header *hdr)
     size_t signature_length;
     bool verified = false;
     uint8_t header_digest[64];
+    struct bpak_meta_header *mh;
 
     rc = bpak_valid_header(hdr);
 
@@ -123,6 +138,52 @@ int boot_image_auth_header(struct bpak_header *hdr)
     } else {
         LOG_ERR("Authentication failed");
         return -PB_ERR_AUTHENTICATION_FAILED;
+    }
+
+    bpak_foreach_part(hdr, p) {
+        if (!p->id)
+            break;
+
+        rc = bpak_get_meta(hdr, BPAK_ID_PB_LOAD_ADDR, p->id, &mh);
+
+        if (rc != BPAK_OK) {
+            LOG_ERR("Could not read pb-entry for part %x", p->id);
+            rc = -PB_ERR_BAD_META;
+            break;
+        }
+
+        uintptr_t load_addr = (uintptr_t) *bpak_get_meta_ptr(hdr, mh, uint64_t);
+        size_t bytes_to_read = bpak_part_size(p);
+
+        if (CHECK_OVERLAP(load_addr, bytes_to_read, stack_start, stack_end)) {
+            rc = -PB_ERR_MEM;
+            LOG_ERR("image overlapping with stack segment");
+            break;
+        }
+
+        if (CHECK_OVERLAP(load_addr, bytes_to_read, data_start, data_end)) {
+            rc = -PB_ERR_MEM;
+            LOG_ERR("image overlapping with data segment");
+            break;
+        }
+
+        if (CHECK_OVERLAP(load_addr, bytes_to_read, bss_start, bss_end)) {
+            rc = -PB_ERR_MEM;
+            LOG_ERR("image overlapping with bss");
+            break;
+        }
+
+        if (CHECK_OVERLAP(load_addr, bytes_to_read, code_start, code_end)) {
+            rc = -PB_ERR_MEM;
+            LOG_ERR("image overlapping with code segment");
+            break;
+        }
+
+        if (CHECK_OVERLAP(load_addr, bytes_to_read, no_init_start, no_init_end)) {
+            rc = -PB_ERR_MEM;
+            LOG_ERR("image overlapping with no_init segment");
+            break;
+        }
     }
 
     return rc;
@@ -228,7 +289,9 @@ int boot_image_load_and_hash(struct bpak_header *hdr,
             return rc;
     }
 
-    return hash_final(payload_digest, payload_digest_size);
+    if (rc == PB_OK)
+        return hash_final(payload_digest, payload_digest_size);
+    return rc;
 }
 
 int boot_image_verify_payload(struct bpak_header *hdr,
