d768e14f8fff908e2eba188dae96d162f2978cca
Jonas Blixt <jonpe960@gmail.com>
2023-04-13 13:07:12 +0200

    main: Only print timestamps when config is on

--- src/main.c
+++ src/main.c
@@ -26,12 +26,6 @@ void main(void)
     rc = plat_init();
 
     if (rc != PB_OK)
-        // TODO: Here we should panic. It's possible that:
-        // 1. We don't have console
-        // 2. WDT is not enabled
-        // 3. Systick is not working
-        //
-        // plat_reset?
         goto enter_command_mode;
 
     rc = plat_board_init();
@@ -47,7 +41,9 @@ void main(void)
 
 enter_command_mode:
     printf("Boot aborted (%i), entering command mode\n\r", rc);
+#ifdef CONFIG_PRINT_TIMESTAMPS
     ts_print();
+#endif
 #ifdef CONFIG_CM
     cm_run();
 #endif
