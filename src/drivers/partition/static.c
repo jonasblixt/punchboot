f152d1a75f3102cffc88cf19b7d3628e9be8f1bc
Jonas Blixt <jonpe960@gmail.com>
2023-12-10 15:18:54 +0100

    drivers: part: static: Make 'first_lba' optional
    If first_lba is not set, the static partition map driver will take the
    next available lba as start for the next partition.

--- src/drivers/partition/static.c
+++ src/drivers/partition/static.c
@@ -5,19 +5,26 @@
 int static_ptbl_init(bio_dev_t dev, const struct static_part_table *table)
 {
     size_t block_sz = bio_block_size(dev);
+    lba_t next_start_lba = 0;
     const struct static_part_table *entry = table;
 
     if (table == NULL)
         return -PB_ERR_PARAM;
 
     for (; entry->uu != NULL; entry++) {
-        lba_t last_lba = entry->first_lba + (entry->size / block_sz) - 1;
+        if (entry->first_lba) {
+            next_start_lba = entry->first_lba;
+        }
+
+        lba_t last_lba = next_start_lba + (entry->size / block_sz) - 1;
         bio_dev_t new_dev = bio_allocate_parent(
-            dev, entry->first_lba, last_lba, block_sz, entry->uu, entry->description);
+            dev, next_start_lba, last_lba, block_sz, entry->uu, entry->description);
         if (new_dev < 0) {
             LOG_ERR("bio alloc failed (%i)", new_dev);
             return new_dev;
         }
+
+        next_start_lba = last_lba + 1;
     }
 
     return PB_OK;
